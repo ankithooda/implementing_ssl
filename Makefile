8f6cfdf55b092fb27a2cbb9e4a100c411d304ede
Ankit <ankithooda23@gmail.com>
2024-06-22 03:07:23 +0530

    Chapter 1 : Base64 encoder impl

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ http_client:
 	mkdir -p bin && gcc -g -o bin/http_client http.c
 
 base64:
-	mkdir -p bin && gcc -g -o bin/base64 base64.c
+	mkdir -p bin && gcc -g -o bin/base64 base64.c -lm
 
--- base64.c
+++ base64.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 static char *base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
@@ -10,31 +13,37 @@ void base64_encode(char *src, int n, char *dest) {
   // i tracks the index in the src.
   // j tracks the index in the dest.
   int j = 0;
-  int i = 0;
+  /* int i = 0; */
 
-  /* for (int i = 0; i < n; i++) { */
+  for (int i = 0; i < n; i = i + 3) {
 
     // Three bytes in src encodes to 4 bytes in dest.
 
     // First byte of dest
-    dest[j] = base64[ (src[i] & 0XFC) >> 2];
-    j++;
-    i++;
+    dest[j++] = base64[ (src[i] & 0XFC) >> 2];
 
     // Second byte of dest
-    dest[j] = base64[ (src[i-1] & 0x3) << 4 | (src[i] & 0xF0) >> 4 ];
-    j++;
-    i++;
+    if (i + 1 < n) {
+      dest[j++] = base64[ (src[i] & 0x3) << 4 | (src[i + 1] & 0xF0) >> 4 ];
+    } else {
+      dest[j++] = base64[ (src[i] & 0x3) << 4 ];
+      dest[j++] = '=';
+      dest[j++] = '=';
+      break;
+    }
 
     // Third byte of dest
-    dest[j] = base64[ (src[i-1] & 0x0F) << 2 | (src[i] & 0xC0) >> 6 ];
-    j++;
+    if (i + 2 < n) {
+      dest[j++] = base64[ (src[i+1] & 0x0F) << 2 | (src[i+2] & 0xC0) >> 6 ];
+    } else {
+      dest[j++] = base64[ (src[i+1] & 0x0F) << 2 ];
+      dest[j++] = '=';
+      break;
+    }
 
     // Fourth byte of dest
-    dest[j] = base64[ src[i] & 0x3F ];
-
-
-  /* } */
+    dest[j++] = base64[ src[i+2] & 0x3F ];
+  }
 
 }
 
@@ -42,16 +51,21 @@ void base64_encode(char *src, int n, char *dest) {
 
 /* } */
 
-int main() {
-  char *s = "ABC";
-  char d[5];
+int main(int argc, char *argv[]) {
 
-  char x = 'X';
+  if (argc < 2) {
+    fprintf(stderr, "Usage : base64 <data to be decoded>\n");
+    exit(1);
+  }
+  int src_len = strlen(argv[1]);
+  int dest_len = 4 * (int)ceil((double)src_len / 3);
+  char *dest = (char*)malloc(dest_len);
 
-  printf("%ld - %ld \n", sizeof(x), sizeof(s));
+  printf("Allocated buffer of len %d\n", dest_len);
 
-  base64_encode(s, 3, d);
-  printf("%s\n", s);
-  printf("%s\n", d);
+  base64_encode(argv[1], src_len, dest);
+  printf("%s\n", argv[1]);
+  printf("%s\n", dest);
+  free(dest);
   return 0;
 }
