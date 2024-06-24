c65157c35eb6f0017ba9001307ef49c6dc6d8cc3
Ankit <ankithooda23@gmail.com>
2024-06-24 23:49:55 +0530

    Chapter 01 & 03 : huge.c and base64.c

--- base64.c
+++ base64.c
@@ -4,6 +4,18 @@
 #include <math.h>
 
 static char *base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static int unbase64[] = {
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52,
+  53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 0, -1, -1, -1,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+  16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1,
+  26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1
+
+};
+
 
 // Assume enough memory in dest
 void base64_encode(char *src, int n, char *dest) {
@@ -47,9 +59,9 @@ void base64_encode(char *src, int n, char *dest) {
 
 }
 
-/* void base64_decode(char *src, int n, char *dest) { */
+void base64_decode(char *src, int n, char *dest) {
 
-/* } */
+}
 
 int main(int argc, char *argv[]) {
 
