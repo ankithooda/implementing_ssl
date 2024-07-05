88d255650e80c2b0d52f5c1bebb676e51fd5555f
Ankit <ankithooda23@gmail.com>
2024-07-05 20:00:26 +0530

    Chapter 2 : Fixed wrong entries in intial and final permute tables and added test file

--- chapter_2/des_test.c
+++ chapter_2/des_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// This does not return a 1 for a 1 bit; it just returns non-zero
 #define GET_BIT( array, bit ) \
     ( array[ ( int ) ( bit / 8 ) ] & ( 0x80 >> ( bit % 8 ) ) )
 #define SET_BIT( array, bit ) \
@@ -16,48 +17,54 @@ static void xor( unsigned char *target, const unsigned char *src, int len )
   }
 }
 
-// Initial Permutation
-
-static void permute( unsigned char *target, const unsigned char *src, const int *permute_table, const int len ) {
-
-  for ( int i = 0; i < len * 8; i++ ) {
-    int dest_bit = GET_BIT(src, (permute_table[ i ] - 1));
-
-
-    //printf("------------\nPermute Entry %d - %d - %d\n", i, permute_table[i]-1, dest_bit);
-    //printf("Dest Bit was %d\n", dest_bit);
-    if ( dest_bit ) {
-      //printf("Settting target bit %d\n", i);
+/**
+ * Implement the initial and final permutation functions. permute_table
+ * and target must have exactly len and len * 8 number of entries,
+ * respectively, but src can be shorter (expansion function depends on this).
+ * NOTE: this assumes that the permutation tables are defined as one-based
+ * rather than 0-based arrays, since they're given that way in the
+ * specification.
+ */
+static void permute( unsigned char target[],
+           const unsigned char src[],
+           const int permute_table[],
+           int len )
+{
+  int i;
+  for ( i = 0; i < len * 8; i++ )
+  {
+    if ( GET_BIT( src, ( permute_table[ i ] - 1 ) ) )
+    {
       SET_BIT( target, i );
-    } else {
-      //printf("Clearing target bit %d\n", i);
+    }
+    else
+    {
       CLEAR_BIT( target, i );
     }
   }
 }
 
 static const int initial_permute_table[] = {
-  58, 50, 42, 34, 26, 18, 10, 2,
-  60, 52, 44, 36, 28, 20, 12, 4,
-  62, 54, 46, 38, 30, 22, 14, 6,
-  64, 56, 48, 40, 32, 24, 16, 8,
-  57, 49, 41, 33, 25, 17, 9,  1,
-  59, 51, 43, 35, 27, 19, 11, 3,
-  61, 53, 45, 37, 29, 21, 13, 5,
-  63, 55, 47, 39, 31, 23, 15, 7,
-};
+   58, 50, 42, 34, 26, 18, 10, 2,
+   60, 52, 44, 36, 28, 20, 12, 4,
+   62, 54, 46, 38, 30, 22, 14, 6,
+   64, 56, 48, 40, 32, 24, 16, 8,
+   57, 49, 41, 33, 25, 17, 9,  1,
+   59, 51, 43, 35, 27, 19, 11, 3,
+   61, 53, 45, 37, 29, 21, 13, 5,
+   63, 55, 47, 39, 31, 23, 15, 7 };
 
-static const int final_permute_table[] = {
-  40,  8, 48, 16, 56, 24, 64, 32,
-  39,  7, 47, 15, 55, 23, 63, 31,
-  38,  6, 46, 14, 54, 22, 62, 30,
-  37,  5, 45, 13, 53, 21, 61, 29,
-  36,  4, 44, 12, 52, 20, 60, 28,
-  35,  3, 43, 11, 51, 19, 59, 27,
-  34,  2, 42, 10, 50, 18, 58, 26,
-  33,  1, 41,  9, 49, 17, 57, 25,
-
-};
+/**
+ * This just inverts ip_table.
+ */
+static const int final_permute_table[] = { 40, 8, 48, 16, 56, 24, 64, 32,
+                                39, 7, 47, 15, 55, 23, 63, 31,
+                                38, 6, 46, 14, 54, 22, 62, 30,
+                                37, 5, 45, 13, 53, 21, 61, 29,
+                                36, 4, 44, 12, 52, 20, 60, 28,
+                                35, 3, 43, 11, 51, 19, 59, 27,
+                                34, 2, 42, 10, 50, 18, 58, 26,
+                                33, 1, 41,  9, 49, 17, 57, 25 };
 
 void print_ascii_data( unsigned char *data, int len ) {
   for ( int i = 0; i < len; i++ ) {
