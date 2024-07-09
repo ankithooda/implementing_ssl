873048cdea0b0e514e62d7ca5354346b81850b37
Ankit <ankithooda23@gmail.com>
2024-07-09 23:41:32 +0530

    Chapter 02 : DES

--- chapter_2/des.c
+++ chapter_2/des.c
@@ -23,14 +23,9 @@ static void permute( unsigned char *target, const unsigned char *src, const int 
   for ( int i = 0; i < len * 8; i++ ) {
     int dest_bit = GET_BIT(src, (permute_table[ i ] - 1));
 
-
-    //printf("------------\nPermute Entry %d - %d - %d\n", i, permute_table[i]-1, dest_bit);
-    //printf("Dest Bit was %d\n", dest_bit);
     if ( dest_bit ) {
-      //printf("Settting target bit %d\n", i);
       SET_BIT( target, i );
     } else {
-      //printf("Clearing target bit %d\n", i);
       CLEAR_BIT( target, i );
     }
   }
@@ -41,7 +36,7 @@ static const int initial_permute_table[] = {
   60, 52, 44, 36, 28, 20, 12, 4,
   62, 54, 46, 38, 30, 22, 14, 6,
   64, 56, 48, 40, 32, 24, 16, 8,
-  57, 49, 41, 33, 25, 17, 9,  1,
+  57, 49, 41, 33, 25, 17,  9, 1,
   59, 51, 43, 35, 27, 19, 11, 3,
   61, 53, 45, 37, 29, 21, 13, 5,
   63, 55, 47, 39, 31, 23, 15, 7,
@@ -56,8 +51,54 @@ static const int final_permute_table[] = {
   35,  3, 43, 11, 51, 19, 59, 27,
   34,  2, 42, 10, 50, 18, 58, 26,
   33,  1, 41,  9, 49, 17, 57, 25,
-
 };
+
+static const int key_permutation_1[] = {
+  57, 49, 41, 33, 25, 17,  9,  1,
+  58, 50, 42, 34, 26, 18, 10,  2,
+  59, 51, 43, 35, 27, 19, 11,  3,
+  60, 52, 44, 36, 63, 55, 47, 39,
+  31, 23, 15,  7, 62, 54, 46, 38,
+  30, 22, 14,  6, 61, 53, 45, 37,
+  29, 21, 13,  5, 28, 20, 12,  4,
+};
+
+static const int key_permutation_2[] = {
+  14, 17, 11, 24,  1,  5,  3, 28,
+  15,  6, 21, 10, 23, 19, 12,  4,
+  26,  8, 16,  7, 27, 20, 13,  2,
+  41, 52, 31, 37, 47, 55, 30, 40,
+  51, 45, 33, 48, 44, 49, 39, 56,
+  34, 53, 46, 42, 50, 36, 29, 32,
+};
+
+// Rotates the 56-bit key into two 28-bit parts
+static void rol( unsigned char *target, int c ) {
+
+  int left_carry, right_carry;
+
+  left_carry = right_carry = 0;
+
+  for ( int i = 0; i < c; i++ ) {
+    // Rotate the first 28-bits
+
+    left_carry = (target[0] & 0x80) >> 3;
+
+    target[0] = (target[0] << 1) | ((target[1] & 0x80) >> 7);
+    target[1] = (target[1] << 1) | ((target[2] & 0x80) >> 7);
+    target[2] = (target[2] << 1) | ((target[3] & 0x80) >> 7);
+
+    right_carry = (target[3] >> 3) & 0x08;
+
+    target[3] = (target[3] << 1) | ((target[4] & 0x80) >> 7);
+    target[3] = target[3] | (left_carry >> 3);
+
+    target[4] = (target[4] << 1) | ((target[5] & 0x80) >> 7);
+    target[5] = (target[5] << 1) | ((target[6] & 0x80) >> 7);
+    target[6] = (target[6] << 1) | right_carry;
+  }
+}
+
 
 void print_ascii_data( unsigned char *data, int len ) {
   for ( int i = 0; i < len; i++ ) {
@@ -72,6 +113,9 @@ void print_hex_data( unsigned char *data, int len ) {
   printf("\n");
 }
 
+static void des_block_operate() {
+
+}
 
 int main() {
 
@@ -87,18 +131,21 @@ int main() {
   plaintext[4] = 104;
   plaintext[5] = 111;
   plaintext[6] = 111;
-  plaintext[7] = 100;
+  //plaintext[7] = 100;
 
+  print_hex_data(plaintext, 7);
+  rol(plaintext, 1);
+  print_hex_data(plaintext, 7);
 
-  permute( enc_text, plaintext, initial_permute_table, block_len );
+  /* permute( enc_text, plaintext, initial_permute_table, block_len ); */
 
-  print_ascii_data( plaintext, block_len );
-  print_hex_data( plaintext, block_len );
+  /* print_ascii_data( plaintext, block_len ); */
+  /* print_hex_data( plaintext, block_len ); */
 
-  print_hex_data( enc_text, block_len );
-  permute( final_text, enc_text, final_permute_table, block_len );
-  print_hex_data( final_text, block_len );
-  print_ascii_data( final_text, block_len );
+  /* print_hex_data( enc_text, block_len ); */
+  /* permute( final_text, enc_text, final_permute_table, block_len ); */
+  /* print_hex_data( final_text, block_len ); */
+  /* print_ascii_data( final_text, block_len ); */
 
   return 0;
 }
